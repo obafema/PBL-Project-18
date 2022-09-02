80a10277584f31227f870ff00bf20d78b9f448a1
obafema <obafema@gmail.com>
2022-09-02 10:39:53 +0100

    updating backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
+terraform {
+ backend "s3" {
 #     bucket         = "pbl-project-18"
 #     key            = "global/s3/terraform.tfstate"
 #     region         = "us-east-2"
 #     dynamodb_table = "terraform-locks"
-#     encrypt        = true
-#   }
-# }
+#     encrypt        = true#   }
+}
+}
